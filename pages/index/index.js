a2f34f1421e86d522155ec298b2bd8edd5ac10b6
yangqifang <32587968+yangqifang@users.noreply.github.com>
2020-07-20 13:40:03 +0800

    第一次提交

---
+++ pages/index/index.js
@@ -0,0 +1,44 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    position:{
+      longitude:0.00,
+      latitude:0.00
+    },
+    //是否开启卫星地图
+    satellite:false,
+    //是否开启实施路况
+    traffic:false,
+  },
+  onLoad: function () {
+    let self=this
+    wx.getLocation({
+      type: 'wgs84',
+      altitude: true,
+      isHighAccuracy:true,
+      success (res){
+        self.setData({
+          position:{
+            latitude:res.latitude,
+            longitude:res.longitude
+          }
+        })
+      }
+    })
+    wx.startLocationUpdate({
+      success: (res) => {
+        wx.onLocationChange((result) => {
+          console.log(result)
+        })
+      },
+    })
+  },
+  report:function(){
+    wx.navigateTo({
+      url: '/pages/report/report',
+    })    
+  }
+})
---
+++ pages/report/report.js
@@ -0,0 +1,83 @@
+// pages/report.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    longitude:0.00,
+    latitude:0.00,
+    address:''
+  },
+
+  options: {
+    addGlobalClass: true
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '上报路况',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  /**
+   * 选择位置
+   */
+  onChangeAddress:function(){
+    wx.chooseLocation({
+      success:function(res){
+        console.log(res)
+      }
+    })
+  }
+})
