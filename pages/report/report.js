0976ea18bc0614ef68715639c7e6cac6c98ed6cb
yangqifang <32587968+yangqifang@users.noreply.github.com>
2020-07-21 19:57:32 +0800

    上报功能JS

--- pages/report/report.js
+++ pages/report/report.js
@@ -5,9 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    longitude:0.00,
-    latitude:0.00,
-    address:''
+    longitude:0.00,//经度
+    latitude:0.00,//维度
+    address:'',//地址
+    description:''//描述
   },
 
   options: {
@@ -74,10 +75,46 @@ Page({
    * 选择位置
    */
   onChangeAddress:function(){
+    let that=this
     wx.chooseLocation({
       success:function(res){
         console.log(res)
+        that.setData({
+          address:res.address,
+          longitude:res.longitude,
+          latitude:res.latitude
+        })
       }
     })
+  },
+  /**
+   * 用户点击上报
+   */
+  onUpload:function(){
+    if(!this.data.address||!this.data.latitude||!this.data.longitude){
+      wx.showToast({
+        title: '请选择或者输入位置信息',
+        icon:'none'
+      })
+      return;
+    }
+    if(!this.data.description){
+      wx.showToast({
+        title: '请说明当前位置发生了什么',
+        icon:'none'
+      })
+      return;
+    }
+
+    wx.showToast({
+      title: '上报成功！感谢你的上报',
+      duration:5000,
+      success:function(){
+        wx.navigateBack({
+          delta: 1
+        })
+      }
+    })
+    
   }
 })
